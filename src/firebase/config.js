32c07ae5f479444ff05dbebfa265e5db63fa4ade
Sairoden <odingandarosa@gmail.com>
2023-05-01 13:48:17 +0800

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJZy3HO-4rZWPXEMHDY3gCVvWL73YZA_4",
+  authDomain: "reading-list-e061d.firebaseapp.com",
+  projectId: "reading-list-e061d",
+  storageBucket: "reading-list-e061d.appspot.com",
+  messagingSenderId: "362185302828",
+  appId: "1:362185302828:web:ed677812f9774aa4e7c289",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
