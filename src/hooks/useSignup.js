757e0a0e7769ba662f8b53fc6509626c07e64f8a
Sairoden <odingandarosa@gmail.com>
2023-05-08 17:12:39 +0800

    Sign up user

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+// firebase imports
+import { auth } from "../firebase/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+
+  const signup = async (email, password) => {
+    setError(null);
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      console.log("User signed up:", res);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return { error, signup };
+};
